3890c02cadde0390c68422ed70837e5bf5496c29
Abdo <abdopersonalacc@gmail.com>
2026-03-09 01:19:16 +0530

    homepage partially done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
+import Layout from './components/Layout'
 import './App.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './Pages/Homepage'
+import Movie from './Pages/Movie'
 function App() {
   const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Homepage />
+        },
+        {
+          path: "/movie",
+          element: <Movie />
+        }
+      ]
+    }
+  ])
   return (
     <>
-      <Navbar />
-      {/* Rest of the app will go here */}
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Cards from '../components/Cards'
+
+const Homepage = () => {
+    const [moviesByCategory, setMoviesByCategory] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    const apiKey = import.meta.env.VITE_APP_API_KEY;
+    const categories = {
+        // popular: "/movie/popular",
+        action: "/discover/movie?with_genres=28",
+        adventure: "/discover/movie?with_genres=12",
+        animation: "/discover/movie?with_genres=16",
+        comedy: "/discover/movie?with_genres=35",
+        crime: "/discover/movie?with_genres=80",
+        documentary: "/discover/movie?with_genres=99",
+        drama: "/discover/movie?with_genres=18",
+        fantasy: "/discover/movie?with_genres=14",
+        horror: "/discover/movie?with_genres=27",
+        mystery: "/discover/movie?with_genres=9648",
+        romance: "/discover/movie?with_genres=10749",
+        sciFi: "/discover/movie?with_genres=878",
+        thriller: "/discover/movie?with_genres=53"
+    };
+    const fetchMovies = async () => {
+        try {
+            setLoading(true);
+
+            const results = {};
+
+            for (const [category, endpoint] of Object.entries(categories)) {
+                console.log(apiKey)
+                const res = await fetch(
+                    `https://api.themoviedb.org/3${endpoint}&api_key=${apiKey}`
+                );
+
+                const data = await res.json();
+                results[category] = data.results;
+            }
+
+            setMoviesByCategory(results);
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    // TODO: handle search
+
+
+    // TODO: handle pagination
+
+    return (
+        <div>
+            {/* TODO: Genre, Year, Rating filter */}
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>Error occurred while fetching movies.</p>
+            ) : (
+                Object.entries(moviesByCategory).map(([category, movies]) => (
+                    <div key={category} className="category-section">
+                        <h2 className="category-title">{category}</h2>
+                        <Cards movies={movies} />
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
+
+export default Homepage
